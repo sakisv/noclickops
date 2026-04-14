e1d2db7e7e6bcec1afa7d4fe61f35bc8e2f21339
sakisv <me@sakisv.net>
2026-04-14 18:41:59 +0300

    Add meta in the output

--- filter.go
+++ filter.go
@@ -2,21 +2,24 @@ package main
 
 import "github.com/noclickops/common"
 
-func filter(managedIds map[string]struct{}, foundRecords map[string][]common.Resource) map[string][]common.Resource {
-	unmanagedResourceIds := make(map[string][]common.Resource)
+func filter(managedIds map[string]struct{}, foundRecords map[string][]common.Resource) map[string]common.FilteredResults {
+	unmanagedResources := make(map[string]common.FilteredResults)
 	for key, value := range foundRecords {
 		if len(value) == 0 {
 			continue
 		}
+		entry := unmanagedResources[key]
 		for _, el := range value {
+			entry.Meta.Found += 1
 			_, found := managedIds[el.TerraformID]
 			if found {
-				//println("[DEBUG] Found " + el)
+				entry.Meta.Managed += 1
 			} else {
-				unmanagedResourceIds[key] = append(unmanagedResourceIds[key], el)
-				//println("[DEBUG] Not found " + el)
+				entry.Resources = append(entry.Resources, el)
+				entry.Meta.Unmanaged += 1
 			}
 		}
+		unmanagedResources[key] = entry
 	}
-	return unmanagedResourceIds
+	return unmanagedResources
 }
